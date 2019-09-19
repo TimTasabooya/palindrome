aa34945da65259e7efe837782eb5b2cd2825b484
timtasabia@agentology.com <timtasabia@gmail.com>
2019-09-18 18:54:58 -0700

    first version of the palindrome checker function complete

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 
-// Here lies the palindrome checking function. 
+// Here lies the palindrome checking function. Be amazed.
 
 function isPalindrome(str) {
-	for (var i = 0; i < str.length; i++) {
-		
+	var revStr = ''; // first create an empty string
+	
+	// then iterate through the original string, starting from the last letter and working backwards
+	for (var i = str.length-1; i >= 0; i--) { 
+		revStr = revStr + str[i]; // at each index, add the letter to the empty string, eventually creating a reversed string
 	}
+	// lastly compare the original string with the reversed string.
+	// If they are equal, then the word is a palindrome and return true. Else false.
+	return str === revStr;
 }
+
+
+console.log(isPalindrome('dooode'));
