7e0d6cd67f4dc95321613031f3d2a82554b90051
timtasabia@agentology.com <timtasabia@gmail.com>
2019-09-23 18:26:55 -0700

    finished funcitonality, now just need to add style

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-
 // Here lies the palindrome checking function. Be amazed.
 
-	// To be safe, let's convert the input to a string (incase an int is passed in)
-	// now create an new empty string
-	// then iterate through the original string, starting from the last letter and working backwards
-	// at each index, add the letter to the empty string, eventually creating a reversed string
-	// lastly compare the original string with the reversed string.
-	// If they are equal, then the word is a palindrome and return true. Else false.
+// To be safe, let's convert the input to a string (incase an int is passed in)
+// now create an new empty string
+// then iterate through the original string, starting from the last letter and working backwards
+// at each index, add the letter to the empty string, eventually creating a reversed string
+// lastly compare the original string with the reversed string.
+// If they are equal, then the word is a palindrome and return true. Else false.
 
 function isPalindrome() {
-	var str = document.getElementById("userInput").value;
-	var reverseString = '';
-	for (var i = str.length-1; i >= 0; i--) { 
-		reverseString = reverseString + str[i]; 
-	}
+    var str = document.getElementById("userInput").value;
+    var reverseString = '';
+    for (var i = str.length - 1; i >= 0; i--) {
+        reverseString = reverseString + str[i];
+    }
 
-	if (reverseString == str) {
-		var node = document.createElement("LI");
-		var textnode = document.createTextNode(str);
-		// node.appendChild(textnode);
-		document.getElementById("isPal").appendChild(textnode).value;
-	} else {
-		var node = document.createElement("LI");
-		var textnode = document.createTextNode(str);
-		// node.appendChild(textnode);
-		document.getElementById("notPal").appendChild(textnode).value;
-	}
+    // Create a new list item and prepare it to be placed
+    var node = document.createElement("LI");
+    var textnode = document.createTextNode(str);
+    node.appendChild(textnode);
+
+    // Compare the reverseString to the original str. 
+    // If they match then place the new list item into the correct list.
+    if (reverseString == str) {
+        document.getElementById("isPal").appendChild(node).value;
+    } else {
+        document.getElementById("notPal").appendChild(node).value;
+    }
 }
 
 
